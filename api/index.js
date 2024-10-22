bd253c1feecc1d21f99edf561e2b13be8e98128f
Chandrasekar.Rajagopal <Chandrasekar.Rajagopal@parker.com>
2024-10-22 17:43:11 +0530

    header update

--- api/index.js
+++ api/index.js
@@ -29,6 +29,27 @@ app.get('/videoinline', (req, res) => {
     res.setHeader('Content-Type', 'video/mp4');
     res.setHeader('Content-Disposition', `inline; filename="${videoName}"`);
 
+    res.setHeader('Cache-Control', 'max-age=148777');
+    res.setHeader('Content-Disposition', 'inline'); // Change 'attachment' to 'inline' as requested
+    res.setHeader('Content-Security-Policy', "upgrade-insecure-requests; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'");
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Date', new Date().toUTCString()); // Automatically set current date
+    res.setHeader('ETag', '"342a70-5ea577eaeea00-gzip"');
+    res.setHeader('Expires', 'Thu, 24 Oct 2024 04:44:09 GMT'); // Set static date or manage dynamically
+    res.setHeader('Last-Modified', 'Thu, 06 Oct 2022 06:06:32 GMT');
+    res.setHeader('Permissions-Policy', 'accelerometer=(self), camera=(self), geolocation=(self), gyroscope=(self), magnetometer=(self), microphone=(self), payment=(self), usb=(self)');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    res.setHeader('Vary', 'Accept-Encoding');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Dispatcher', 'dispatcher2eastus2');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('Server-Timing', 'cdn-cache; desc=HIT');
+    res.setHeader('Server-Timing', 'edge; dur=3');
+    res.setHeader('Server-Timing', 'ak_p; desc="1729596272284_388762143_745994121_313_23280_10_0_146"; dur=1');
+    res.setHeader('X-VHost', 'parkercompublish');
+    
+
     // Stream the video file
     res.sendFile(videoPath);
 });
@@ -40,6 +61,26 @@ app.get('/videoattachment', (req, res) => {
     // Set the headers for Content-Disposition as inline
     res.setHeader('Content-Type', 'video/mp4');
     res.setHeader('Content-Disposition', `attachment; filename="${videoName}"`);
+
+    res.setHeader('Cache-Control', 'max-age=148777');
+    res.setHeader('Content-Disposition', 'inline'); // Change 'attachment' to 'inline' as requested
+    res.setHeader('Content-Security-Policy', "upgrade-insecure-requests; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'");
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Date', new Date().toUTCString()); // Automatically set current date
+    res.setHeader('ETag', '"342a70-5ea577eaeea00-gzip"');
+    res.setHeader('Expires', 'Thu, 24 Oct 2024 04:44:09 GMT'); // Set static date or manage dynamically
+    res.setHeader('Last-Modified', 'Thu, 06 Oct 2022 06:06:32 GMT');
+    res.setHeader('Permissions-Policy', 'accelerometer=(self), camera=(self), geolocation=(self), gyroscope=(self), magnetometer=(self), microphone=(self), payment=(self), usb=(self)');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    res.setHeader('Vary', 'Accept-Encoding');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Dispatcher', 'dispatcher2eastus2');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('Server-Timing', 'cdn-cache; desc=HIT');
+    res.setHeader('Server-Timing', 'edge; dur=3');
+    res.setHeader('Server-Timing', 'ak_p; desc="1729596272284_388762143_745994121_313_23280_10_0_146"; dur=1');
+    res.setHeader('X-VHost', 'parkercompublish');
 
     // Stream the video file
     res.sendFile(videoPath);
